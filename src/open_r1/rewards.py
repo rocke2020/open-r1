3d378be15353492cd1ab788df5a60746a8e2ba8a
qichang.dong <dongqichang@ecmas.ai>
2025-02-16 11:57:46 +0800

    Fix len_reward function type

--- src/open_r1/rewards.py
+++ src/open_r1/rewards.py
@@ -75,7 +75,7 @@ def reasoning_steps_reward(completions, **kwargs):
     return [min(1.0, count / 3) for count in matches]
 
 
-def len_reward(completions: list[Dict[str, str]], solutions: list[str], **kwargs) -> float:
+def len_reward(completions: list[Dict[str, str]], solutions: list[str], **kwargs) -> list[float]:
     """Compute length-based rewards to discourage overthinking and promote token efficiency.
 
     Taken from from the Kimi 1.5 tech report: https://arxiv.org/abs/2501.12599
